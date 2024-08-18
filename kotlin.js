b47c6cf515a3ab28e193beb87a11a6aa14ca0646
Serafina Panagiotaki <std167437@ac.eap.gr>
2024-08-18 11:48:48 +0300

    Add files via upload

---
+++ kotlin.js
@@ -0,0 +1,74 @@
+//var current=document.getElementsByClassName("display_info").innerHTML;
+function gr(current){ 
+    switch(current){
+        case 1:
+            location="kotlin_gr.html";
+            break;
+        case 2:
+            location="kotlin_gr_overview.html";
+            break;
+        case 3:
+            location="kotlin_gr_intro.html";
+            break;
+        case 4:
+            location="kotlin_gr_syntax.html";
+            break;
+        case 5:
+            location="kotlin_gr_functions.html";
+            break;
+        case 6:
+            location="kotlin_gr_oop.html";
+            break; 
+        case 7:
+            location="kotlin_gr_classes.html";
+            break;
+        case 8:
+            location="kotlin_gr_constructors.html";
+            break;  
+        case 9:
+            location="kotlin_gr_methods.html";
+            break; 
+        case 10:
+            location=" kotlin_gr_inheritance.html";
+            break;                                                          
+    }               
+}
+function uk(current){ 
+    switch(current){
+        case 1:
+            location="kotlin_uk.html";
+            break;
+        case 2:
+            location="kotlin_uk_overview.html";
+            break;
+        case 3:
+            location="kotlin_uk_intro.html";
+            break;
+        case 4:
+            location="kotlin_uk_syntax.html";
+            break;
+        case 5:
+            location="kotlin_uk_functions.html";
+            break;
+        case 6:
+            location="kotlin_uk_oop.html";
+            break; 
+        case 7:
+            location="kotlin_uk_classes.html";
+            break;   
+        case 8:
+            location="kotlin_uk_constructors.html";
+            break; 
+        case 9:
+            location="kotlin_uk_methods.html";
+            break;                                              
+        case 10:
+            location="kotlin_uk_inheritance.html";
+            break;                                              
+        } 
+    }                
+
+  
+function info(){
+    alert("Programmed, developed and designed by Serafina Panagiotaki\n\nD-Semester Project on Kotlin program language\n\nCopyright 2023")
+}   
